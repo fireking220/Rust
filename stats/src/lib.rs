1c7a86c65f1eb1a445cc8aff4f9577af65860f64
Scott Patterson <sip2@pdx.edu>
2019-06-28 14:48:01 -0700

    Completed HW1

--- stats/src/lib.rs
+++ stats/src/lib.rs
@@ -9,7 +9,7 @@
 /// Type of statistics function. If the statistic
 /// is ill-defined, `None` will be returned.
 pub type StatFn = fn(&[f64]) -> Option<f64>;
-
+use num::pow;
 /// Arithmetic mean of input values. The mean of an empty
 /// list is 0.0.
 ///
@@ -24,7 +24,22 @@ pub type StatFn = fn(&[f64]) -> Option<f64>;
 /// assert_eq!(Some(0.0), mean(&[-1.0, 1.0]));
 /// ```
 pub fn mean(nums: &[f64]) -> Option<f64> {
-    unimplemented!("no mean yet")
+    //unimplemented!("no mean yet")
+    //Some(1.3_f64)
+    if nums.len() == 0 {
+        return Some(0.0_f64);
+    }
+    let mut my_mean: f64 = 0.0;
+    let len = nums.len() as f64;
+    for x in nums {
+        my_mean += x;
+    }
+    Some(my_mean / len)
+}
+
+#[test]
+fn test_mean_1() {
+    assert_eq!(Some(2.5), mean(&[1.0, 2.0, 3.0, 4.0]));
 }
 
 /// Population standard deviation of input values. The
@@ -41,7 +56,23 @@ pub fn mean(nums: &[f64]) -> Option<f64> {
 /// assert_eq!(Some(0.0), stddev(&[1.0, 1.0]));
 /// ```
 pub fn stddev(nums: &[f64]) -> Option<f64> {
-    unimplemented!("no stddev yet")
+    if nums.len() == 0 {
+        return None;
+    }
+    // stddev of a single element is 0
+    else if nums.len() == 1 {
+        return Some(0.0_f64);
+    }
+    let len = (nums.len() - 1) as f64;
+    let mean = mean(nums).unwrap();
+    let mut sum = 0.0_f64;
+    // sum of squares
+    for x in nums {
+        sum += pow(x - mean, 2);
+    }
+    sum = sum / len;
+    sum = sum.sqrt();
+    Some(sum)
 }
 
 /// Median value of input values, taking the value closer
@@ -63,8 +94,23 @@ pub fn median(nums: &[f64]) -> Option<f64> {
     let mut nums = nums.to_owned();
     // https://users.rust-lang.org/t/how-to-sort-a-vec-of-floats/2838/2
     nums.sort_by(|a, b| a.partial_cmp(b).unwrap());
-
-    unimplemented!("no median yet")
+    let len = nums.len() as i64;
+    let mut ret = 0.0_f64;
+    // if array is empty
+    if len == 0 {
+        return None;
+    }
+    //if array length is even
+    else if len % 2 == 0 {
+        let even = ((len / 2) - 1) as usize;
+        ret = nums[even] as f64;
+    }
+    //if array length is odd
+    else if len % 2 == 1 {
+        let odd = (len / 2) as usize;
+        ret = nums[odd] as f64;
+    }
+    Some(ret)
 }
 
 /// L2 norm (Euclidean norm) of input values. The L2
@@ -81,5 +127,15 @@ pub fn median(nums: &[f64]) -> Option<f64> {
 /// assert_eq!(Some(5.0), l2(&[-3.0, 4.0]));
 /// ```
 pub fn l2(nums: &[f64]) -> Option<f64> {
-    unimplemented!("no l2 yet")
+    if nums.len() == 0 {
+        return Some(0.0_f64);
+    }
+    let mut sum = 0.0_f64;
+    // x gets the address of the element in nums
+    for x in nums {
+        // * dereferences the addess that x points to
+        sum += pow(*x, 2);
+    }
+    sum = sum.sqrt();
+    Some(sum)
 }
